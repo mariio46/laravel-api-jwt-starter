545b53b764be266ee6d8c11021049a6009e9340b
Mario <mariomad2296@gmail.com>
2025-02-12 09:56:24 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/register', Controllers\Auth\RegisterController::class);
 
-Route::post('/auth/login', Controllers\Auth\LoginController::class);
+// Route::post('/auth/login', Controllers\Auth\LoginController::class);
 
-Route::get('/auth/refresh', Controllers\Auth\RefreshTokenController::class)->middleware('auth:api');
+// Route::get('/auth/refresh', Controllers\Auth\RefreshTokenController::class)->middleware('auth:api');
 
-Route::get('/auth/user', Controllers\Auth\AuthenticatedUserController::class)->middleware('auth:api');
+// Route::get('/auth/user', Controllers\Auth\AuthenticatedUserController::class)->middleware('auth:api');
 
-Route::delete('/auth/logout', Controllers\Auth\LogoutController::class)->middleware('auth:api');
+// Route::delete('/auth/logout', Controllers\Auth\LogoutController::class)->middleware('auth:api');
