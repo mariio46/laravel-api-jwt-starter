36e6974563ad8fe1c4f6d73e2d69540310ab8da0
Mario <mariomad2296@gmail.com>
2025-02-12 11:03:16 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,13 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Contracts\AuthContract;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
-use F9Web\ApiResponseHelpers;
 use Illuminate\Http\JsonResponse;
 
 class RegisterController extends Controller
 {
-    use ApiResponseHelpers;
-
     public function __invoke(RegisterRequest $request, AuthContract $authContract): JsonResponse
     {
         $response = $authContract->register($request->only([
