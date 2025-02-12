3e1b32b0277f053a7d384dbd1a9750209225e8bf
Mario <mariomad2296@gmail.com>
2025-02-12 09:31:56 +0800

    Add routes for API endpoints

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/auth/register', Controllers\Auth\RegisterController::class);
+
+Route::post('/auth/login', Controllers\Auth\LoginController::class);
+
+Route::get('/auth/refresh', Controllers\Auth\RefreshTokenController::class)->middleware('auth:api');
+
+Route::get('/auth/user', Controllers\Auth\AuthenticatedUserController::class)->middleware('auth:api');
+
+Route::delete('/auth/logout', Controllers\Auth\LogoutController::class)->middleware('auth:api');
